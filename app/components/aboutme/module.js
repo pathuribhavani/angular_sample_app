c571c819b6bbfe0dede897ddcefe021de965c2b4
pathuribhavani <bavani.pathuri@gmail.com>
2019-05-22 19:39:34 -0500

    comitted

---
+++ app/components/aboutme/module.js
@@ -0,0 +1,11 @@
+
+angular.module("myApp").controller("myController", function ($scope)
+{
+    let Friend= {
+        FirstName: "Bhavani",
+        LastName : "Pathuri",
+        
+    }
+    $scope.Friend=Friend;
+    
+});
